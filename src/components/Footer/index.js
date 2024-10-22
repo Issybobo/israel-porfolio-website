eab3abdc51005549c7767bb68f7d1cf14a2bf901
Israel Ogunsola <110337578+Issybobo@users.noreply.github.com>
2024-10-22 16:05:16 +0100

    fix some issues

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -103,7 +103,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2023 Israel Ogunsola. All rights reserved.
+          &copy; 2024 Israel Ogunsola. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
